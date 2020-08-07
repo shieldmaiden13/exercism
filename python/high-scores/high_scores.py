5a57196399049641e9269e4939fec6221a7aef9c
shieldmaiden13 <seyfi.toma@gmail.com>
2020-08-08 03:29:27 +0400

    added Gitignore, solutions for Exercism: 'high-scores'.

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,16 @@
+
+
+def latest(scores):
+
+    return scores[-1]
+
+
+def personal_best(scores):
+    return max(scores)
+
+
+def personal_top_three(scores):
+    return list(reversed(sorted(scores)))[0:3]
+
+
+
