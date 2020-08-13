0a9c15b1c93ac4b557369697b596a8503d39a9cb
shieldmaiden13 <seyfi.toma@gmail.com>
2020-08-14 00:31:32 +0400

    added two-fer

--- python/rest-api/rest_api.py
+++ python/rest-api/rest_api.py
@@ -1,9 +1,21 @@
+import json
+
+
 class RestAPI:
     def __init__(self, database=None):
-        pass
+        self.database = database
 
     def get(self, url, payload=None):
-        pass
+        self.url = url
+        self.payload = payload
+
+        resp = {"users": []}
+
+        if self.url == "/users":
+            for users in self.payload:
+                return users
+
+
 
     def post(self, url, payload=None):
         pass
